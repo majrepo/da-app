c60bd5d4928825e5e9774b3526a147f4c802bc50
maja maja <trzaska.maja@gmail.com>
2021-05-11 23:23:15 +0200

    Main4

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import Depends, FastAPI, HTTPException, Request, Response, status
 app = FastAPI()
 
-
-@app.api_route(
-    path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"], status_code=200
-)
-def read_request(request: Request, response: Response):
-    request_method = request.method
-    return {"method": request_method}
+@app.get("/")
+def read_root():
+    return {"message": "Hello world!"}
+    
+# @app.api_route(
+#     path="/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"], status_code=200
+# )
+# def read_request(request: Request, response: Response):
+#     request_method = request.method
+#     return {"method": request_method}
